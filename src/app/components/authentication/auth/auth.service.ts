34bc90eb0092ad41a743f5096ec5f0349f336eb2
DreamStore <chinhnd146@gmail.com>
2022-06-15 16:25:13 +0700

    jwt: validate http request

--- src/app/components/authentication/auth/auth.service.ts
+++ src/app/components/authentication/auth/auth.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import {HttpClient,  HttpErrorResponse} from '@angular/common/http';
+import {BehaviorSubject, Observable, throwError} from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+
+import { APP_CONFIG } from 'src/app/shared/common/config';
+import { User } from 'src/app/models/user';
+
+
 @Injectable()
 export class AuthService {
-    constructor(public jwtHelper: JwtHelperService) {}
+    //private currentUserSubject: BehaviorSubject<User>;
+    constructor(public jwtHelper: JwtHelperService, private http: HttpClient) {}
+
+    private handleError(error: HttpErrorResponse) {
+        let error_msg = '';
+        if (error.status === 0) {
+            console.error('An error occurred:', error.error);
+        } else {
+            console.error(`Backend returned code ${error.status}, body was: `, error.error);
+        }
+        return throwError(() => new Error('Something bad happened; please try again later.'));
+    }
+
     public isAuthenticated(): boolean {
         let token = localStorage.getItem('token');
         if(!token){
@@ -11,4 +32,21 @@ export class AuthService {
         //token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
         return !this.jwtHelper.isTokenExpired(token);
     }
+
+    postLogin(user:any) {
+        return this.http.post<User>(`${APP_CONFIG.BASE_API_LINK.BASE}/login.php`, user).pipe(
+            catchError(this.handleError)
+        )
+    }
+
+    //login(user: any): Observable<> {
+    //    return this.http
+    //        .post<any>(`${APP_CONFIG.BASE_API_LINK.BASE}/login`, user)
+    //        .pipe(
+    //            map((res) => {
+    //                this.currentUserSubject.next(res.data);
+    //                return res;
+    //            })
+    //        );
+    //}
 }
--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Router, CanActivate } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from '../../../shared/directive/forbidden-name.directive';
 
 @Component({
   selector: 'app-login',
@@ -9,28 +12,71 @@ import { Router, CanActivate } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  formLogin: any ={}
-  constructor(public jwtHelper: JwtHelperService,  public router: Router) { }
+  formLogin: any = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      forbiddenNameValidator(/bob/i)
+    ]),
+    password: new FormControl('', [Validators.required]),
+  });
+  userData: any ={};
+  submitted = false;
+  msgError = '';
+  constructor(public jwtHelper: JwtHelperService,  public router: Router, public auth: AuthService) { }
 
   ngOnInit(): void {
-
+    this.formLogin.valueChanges.subscribe((currentValue: any) => {
+      this.msgError = '';
+    });
   }
 
-  onLogin(){
-    console.log('login', this.formLogin)
-    if(this.formLogin && this.formLogin.name && this.formLogin.password){
-      if(this.formLogin.name == 'chinhnd' && this.formLogin.password == 'admin'){
-        let new_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
-        localStorage.setItem('token', new_token);
-      }
+  onLogin(): void{
+    this.submitted = true;
+    console.log('login', this.formLogin.valid)
+    if(this.formLogin.valid){
+      this.auth.postLogin(this.formLogin.value).subscribe(
+          (data: any) => {
+            this.submitted = false;
+            if(data.success){
+              this.userData = { ...data };
+              localStorage.setItem('token', data.token);
+              this.router.navigate(['crm']);
+            }else{
+              this.msgError = data.msg
+            }
+          },
+          error => {
+            this.submitted = false;
+            this.msgError = error;
+          }
+      );
     }
-    let token = localStorage.getItem('token');
-    if(token){
-      this.router.navigate(['crm']);
-      if(!this.jwtHelper.isTokenExpired(token)){
-        // do something
-      }
-    }
+    //return;
+    //if(this.formLogin && this.formLogin.name && this.formLogin.password){
+    //  if(this.formLogin.name == 'chinhnd' && this.formLogin.password == 'admin'){
+    //    let new_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+    //    localStorage.setItem('token', new_token);
+    //  }
+    //}
+    //let token = localStorage.getItem('token');
+    //if(token){
+    //  this.router.navigate(['crm']);
+    //  if(!this.jwtHelper.isTokenExpired(token)){
+    //    // do something
+    //  }
+    //}
+  }
 
+  get username(){
+    return this.formLogin.get('username');
+  }
+
+  get password(){
+    return this.formLogin.get('password');
+  }
+
+  ngOnDestroy() {
+    this.formLogin.valueChanges.unsubscribe();
   }
 }
